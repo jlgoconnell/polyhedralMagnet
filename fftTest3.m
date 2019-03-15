2617573caf80a0192eb066a111db1b6d03f2e216
James O'Connell <james.oconnell@adelaide.edu.au>
2019-03-15 19:27:50 +1030

    Fixed integral

--- fftTest3.m
+++ fftTest3.m
@@ -24,15 +24,15 @@ n = 64;
 x = linspace(-0.04,0.04,n);
 y = linspace(-0.04,0.04,n);
 [X,Y] = meshgrid(x,y);
-B = polyhedronField(verticesA,magA,[X(:),Y(:),repmat(0.023,size(X(:)))]);
+B = polyhedronField(verticesA,magA,[X(:),Y(:),repmat(0.02235,size(X(:)))]);
 Bz = reshape(B(:,3),size(X));
 f = Bz;
 
 dx = x(2)-x(1);
 
-figure;
-plot3(X(:),Y(:),f(:),'r.');
-hold on;
+% figure;
+% plot3(X(:),Y(:),f(:),'r.');
+% hold on;
 
 ff = fft2(f);
 % ff = fftshift(ff);
@@ -68,27 +68,27 @@ intt = data;
 A = 2*pi*1i*Freqsx;
 B = 2*pi*1i*Freqsy;
 
-for j = 1:length(x2)
-    j
-    for k = 1:length(y2)
-        
-        xx = x2(j);
-        yy = y2(k);
-        
-        data(k,j) = 1/n^2*sum(sum((ff).*exp(2*pi*1i*Freqsx*(xx-x(1))+2*pi*1i*Freqsy*(yy-y(1)))));
-        intt(k,j) = dx2^2*data(k,j);
-        
-    end
-end
-data = real(data);
+% for j = 1:length(x2)
+%     j
+%     for k = 1:length(y2)
+%         
+%         xx = x2(j);
+%         yy = y2(k);
+%         
+%         data(k,j) = 1/n^2*sum(sum((ff).*exp(2*pi*1i*Freqsx*(xx-x(1))+2*pi*1i*Freqsy*(yy-y(1)))));
+%         intt(k,j) = dx2^2*data(k,j);
+%         
+%     end
+% end
+% data = real(data);
 
 mydata = ifft2(ff);
 
 % f2 = cos(2*pi*1*X2)+2*cos(2*pi*2*Y2+2*pi*3*X2)+3*cos(2*pi*3*Y2);
-surf(X2,Y2,data);
+% surf(X2,Y2,data);
 % plot3(X2(:),Y2(:),f2(:),'ro');
 % surf(X,Y,mydata);
-grid on;
+% grid on;
 
 % error = f-data;
 % figure;
@@ -99,16 +99,18 @@ I = zeros(size(ff));
 % A = 0, B = 0:
 I(1,1) = (x(end)-x(1))*(y(end)-y(1))*ff(1,1);
 % A = 0, B ~= 0:
-I(2:end,1) = ff(2:end,1)*(x(end)-x(1)).*(f(end,1)-f(1,1))./B(2:end,1);
-I(2:end,1) = ff(2:end,1)*(x(end)-x(1)).*(exp(B(2:end,1)*y(end))-exp(B(2:end,1)*y(1)))./B(2:end,1);
+% I(2:end,1) = ff(2:end,1)*(x(end)-x(1)).*(f(end,1)-f(1,1))./B(2:end,1);
+% I(2:end,1) = ff(2:end,1)*(x(end)-x(1)).*(exp(B(2:end,1)*y(end))-exp(B(2:end,1)*y(1)))./B(2:end,1);
+I(2:end,1) = ff(2:end,1)*(x(end)-x(1)).*(exp(B(2:end,1)*(y(end)-y(1)))-1)./B(2:end,1);
 % A ~= 0, B = 0:
-I(1,2:end) = ff(1,2:end)*(y(end)-y(1)).*(f(1,end)-f(1,1))./A(1,2:end);
-I(1,2:end) = ff(1,2:end)*(y(end)-y(1)).*(exp(A(1,2:end)*x(end))-exp(A(1,2:end)*x(1)))./A(1,2:end);
+% I(1,2:end) = ff(1,2:end)*(y(end)-y(1)).*(f(1,end)-f(1,1))./A(1,2:end);
+% I(1,2:end) = ff(1,2:end)*(y(end)-y(1)).*(exp(A(1,2:end)*x(end))-exp(A(1,2:end)*x(1)))./A(1,2:end);
+I(1,2:end) = ff(1,2:end)*(y(end)-y(1)).*(exp(A(1,2:end)*(x(end)-x(1)))-1)./A(1,2:end);
 % A ~= 0, B ~= 0:
-I(2:end,2:end) = ff(2:end,2:end)./(A(2:end,2:end).*B(2:end,2:end))*(f(end,end)-f(1,end)-f(end,1)+f(1,1));
-areaint = sum(abs(I(:)))/n^2
+I(2:end,2:end) = ff(2:end,2:end)./(A(2:end,2:end).*B(2:end,2:end)).*(exp(B(2:end,2:end)*(y(end)-y(1)))-1).*(exp(A(2:end,2:end)*(x(end)-x(1)))-1);
+areaint = abs(sum(I(:)))/n^2
 actualint = trapz(y,trapz(x,f,2),1)
-dataint = trapz(y2,trapz(x2,data,2),1)
+% dataint = trapz(y2,trapz(x2,data,2),1)
 
 
 
