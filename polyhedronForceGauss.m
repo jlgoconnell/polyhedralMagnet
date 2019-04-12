3ae129758b8ecb9d4dc8aee0d097536bc8e6c68f
Unknown <mistermath4@gmail.com>
2019-04-12 16:46:29 +0930

    PolyhedronForceGauss complete
    polyhedronForceGauss is now complete and giving force and torque solutions. For fine meshes, the mesh generation is taking the majority of calculation time, but for coarser meshes, the field calculation is taking the majority of calculation time as expected.

--- polyhedronForceGauss.m
+++ polyhedronForceGauss.m
@@ -1,40 +1,37 @@
 
 
 
-function [F, t] = polyhedronForceGauss(verticesA,verticesB,magA,magB,meshnum)
+function [F,T,t] = polyhedronForceGauss(verticesA,verticesB,magA,magB,meshnum,torquept)
 
 tic;
 
 f = minConvexHull(verticesB);
 [v,~] = surfToMesh(verticesB(:,1),verticesB(:,2),verticesB(:,3));
 [f,~] = triangulateFaces(f);
+MdotN = meshFaceNormals(v,f)*magB';
+f = f(abs(MdotN)>eps,:);
 if meshnum > 1
     [v,f] = subdivideMesh(v,f,meshnum);
 end
-e = meshEdges(f);
 
-% [v2,f2] = subdivideMesh(v,f,2);
+e = zeros(numel(f), 2);
+for i = 1:length(f)
+    ff = f(i, :);
+    e(3*i-2:3*i, :) = [ff' ff([2:end 1])'];
+end
+[e,~,IE] = unique(sort(e, 2),'rows');
+
 MdotN = meshFaceNormals(v,f)*magB';
 areas = meshFaceAreas(v,f);
 
-% Try 2:
-% m = max(f(:));
 midpts = 0.5*(v(e(:,1),:)+v(e(:,2),:));
-fmids = cell2mat(meshFaceEdges(v,e,f));
-
-% a = f2';
-% a = a(a > m);
-% fmids = sort(reshape(a,9,numel(a)/9)',2);
-% fmids = fmids(:,[1,4,7])-m;
+fmids = reshape(IE,3,numel(IE)/3)';
 
 Bfield = polyhedronField(verticesA,magA,midpts);
 
-if sum(isnan(Bfield))>0
-    fprintf('error');
-end
-
 F = sum(Bfield(fmids',:).*repelem(MdotN.*areas,3,1))/(12*pi*10^-7);
-
+myleverpoint = midpts(fmids',:)-torquept;
+T = sum(cross(myleverpoint,Bfield(fmids',:)).*repelem(MdotN.*areas,3,1))/(12*pi*10^-7);
 t = toc;
 
 end
--- test2.m
+++ test2.m
@@ -38,45 +38,93 @@ R_x = [1,0,0;0,cos(thetax),-sin(thetax);0,sin(thetax),cos(thetax)];
 verticesA = verticesA*R_x;
 Sa = alphaShape(verticesA,Inf);
 magA = [0,0,1];
-verticesB = verticesA + repmat([0.00,0.00,0],length(verticesA),1);
-verticesB = verticesB + repmat([0,0,0.01-min(verticesB(:,3))+max(verticesA(:,3))],length(verticesB),1);
+verticesB = verticesA + repmat([0.004,-0.003,0],length(verticesA),1);
+verticesB = verticesB + repmat([0,0,0.001-min(verticesB(:,3))+max(verticesA(:,3))],length(verticesB),1);
 verticesB = verticesB*[cosd(36),-sind(36),0;sind(36),cosd(36),0;0,0,1];
 Sb = alphaShape(verticesB,Inf);
 magB = [0,0,-1];
 
 
-[Fold,~,~,told] = polyhedronForce(verticesA,verticesB,magA,magB,mean(verticesB),1e-50,60);
-Fold(1,:) = [];
+% totaltime = 60;
+% 
+% i = 1;
+% ttotalold = 0;
+% while ttotalold(end) < totaltime
+%     [Fold(i,:),Told(i,:),told(i,1)] = polyhedronForce(verticesA,verticesB,magA,magB,i,mean(verticesB));
+% 	ttotalold = cumsum(told);
+%     i = i + 1;
+% end
+% told = ttotalold;
+% 
+% % [Fold,Told,told] = polyhedronForce(verticesA,verticesB,magA,magB,10,mean(verticesB))
+% % Fold(1,:) = [];
+% % Told(1,:) = [];
+% 
+% i = 1;
+% ttotalgauss = 0;
+% while ttotalgauss(end) < told(end)
+%     [Fgauss(i,:),Tgauss(i,:),tgauss(i,1)] = polyhedronForceGauss(verticesA,verticesB,magA,magB,i,mean(verticesB));
+% 	ttotalgauss = cumsum(tgauss);
+%     i = i + 1;
+% end
+% tgauss = ttotalgauss;
+% 
 
-i = 1;
-ttotal = 0;
-while ttotal(end) < told(end)
-    [Fgauss(i,:),tgauss(i)] = polyhedronForceGauss(verticesA,verticesB,magA,magB,i);
-	ttotal = cumsum(tgauss);
-    i = i + 1;
+for i = 15
+    i
+    [Fold(i,:),Told(i,:),told(i,1)] = polyhedronForce(verticesA,verticesB,magA,magB,i,mean(verticesB));
+    [Fgauss(i,:),Tgauss(i,:),tgauss(i,1)] = polyhedronForceGauss(verticesA,verticesB,magA,magB,i,mean(verticesB));
 end
-tgauss = ttotal;
+told = cumsum(told);
+tgauss = cumsum(tgauss);
 
-figure;
-plot(told,Fold(:,3),'r.-',tgauss,Fgauss(:,3),'b.-');
-legend('Old','New');
-grid on;
 
+
+% 
+% figure;
+% plot(told,Fold(:,3),'r.-',tgauss,Fgauss(:,3),'b.-');
+% legend('Old','New');
+% grid on;
+% 
 errorold = abs(Fold-Fold(end,:));
 errorgauss = abs(Fgauss-Fgauss(end,:));
+erroroldt = abs(Told-Told(end,:));
+errorgausst = abs(Tgauss-Tgauss(end,:));
+% errorold = abs(Fold-f1_xyz');
+% errorgauss = abs(Fgauss-f1_xyz');
+% erroroldt = abs(Told-t1_xyz');
+% errorgausst = abs(Tgauss-t1_xyz');
 figure;
-subplot(1,3,1);
+subplot(2,3,1);
 loglog(told,errorold(:,1),'r.-',tgauss,errorgauss(:,1),'b.-');
 grid on;
 legend('Old','New');
-subplot(1,3,2);
+subplot(2,3,2);
 loglog(told,errorold(:,2),'r.-',tgauss,errorgauss(:,2),'b.-');
 grid on;
 legend('Old','New');
-subplot(1,3,3);
+subplot(2,3,3);
 loglog(told,errorold(:,3),'r.-',tgauss,errorgauss(:,3),'b.-');
 grid on;
 legend('Old','New');
+subplot(2,3,4);
+loglog(told,erroroldt(:,1),'r.-',tgauss,errorgausst(:,1),'b.-');
+grid on;
+legend('Old','New');
+subplot(2,3,5);
+loglog(told,erroroldt(:,2),'r.-',tgauss,errorgausst(:,2),'b.-');
+grid on;
+legend('Old','New');
+subplot(2,3,6);
+loglog(told,erroroldt(:,3),'r.-',tgauss,errorgausst(:,3),'b.-');
+grid on;
+legend('Old','New');
+subplot(2,3,1); title('x-Force error'); xlabel('Time (s)'); ylabel('Error (N)');
+subplot(2,3,2); title('y-Force error'); xlabel('Time (s)'); ylabel('Error (N)');
+subplot(2,3,3); title('z-Force error'); xlabel('Time (s)'); ylabel('Error (N)');
+subplot(2,3,4); title('x-Torque error'); xlabel('Time (s)'); ylabel('Error (Nm)');
+subplot(2,3,5); title('y-Torque error'); xlabel('Time (s)'); ylabel('Error (Nm)');
+subplot(2,3,6); title('z-Torque error'); xlabel('Time (s)'); ylabel('Error (Nm)');
 
 % figure;
 % semilogy(1:length(errorold),errorold,'r.-',1:length(errorgauss),errorgauss,'b.-');
